4d0a988207f3be5036840deb60baf6e43750bab0
Guilherme Henrique Baschera <guilherme.h.baschera@gmail.com>
2023-05-19 09:58:26 -0300

    Carregando estrelas salvas anteriormente
    Ao clicar f11 carrega os pontos que foram salvos no último acesso

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import pygame
+import ast
 from tkinter import simpledialog
-pygame.init()
-#Cores
-branco = (255,255,255)
-preto = (0,0,0)
-#Variáveis
-running = True
-estrelas = {}
 
-#Tela e som
-tamanhoTela = (1800,1000)
+pygame.init()
+# Cores
+branco = (255, 255, 255)
+preto = (0, 0, 0)
+# Variáveis
+running = True
+estrelas = []
+x = 0
+y = 0
+# Tela e som
+tamanhoTela = (1800, 1000)
 espaco = pygame.image.load("espaco1.jpg")
-tela = pygame.display.set_mode( tamanhoTela)
+tela = pygame.display.set_mode(tamanhoTela)
 icone = pygame.image.load("iconeNave.png")
-#Texto das opções
+
+# Texto das opções
 textoX = 20
 textoYF10 = 20
 textoYF11 = 50
@@ -22,10 +26,12 @@ texto = pygame.font.Font(None, 38)
 renderdotextoF10 = texto.render("Pressione F10 para salvar os pontos!", True, branco)
 renderdotextoF11 = texto.render("Pressione F11 para Carregar os pontos!", True, branco)
 renderdotextoF12 = texto.render("Pressione F12 para Deletar os pontos!", True, branco)
+
 pygame.display.set_icon(icone)
 pygame.display.set_caption("Space Marker")
 pygame.mixer.music.load("somEspaco.wav")
 pygame.mixer.music.play(-1)
+
 
 
 while running:
@@ -36,22 +42,33 @@ while running:
             running = False
         elif event.type == pygame.MOUSEBUTTONUP:
 
-            posicao = str(pygame.mouse.get_pos())
+            posicao = (pygame.mouse.get_pos())
+            x, y = posicao
             nomeEstrela = simpledialog.askstring("Space", "Digite o nome da estrela")
             if nomeEstrela == "":
                 nomeEstrela = "Desconhecido"
 
-            with open("posicaoEstrelas.txt", "a") as arquivo:
-                arquivo.write(nomeEstrela + ": " + posicao + "\n")
-            
-            if nomeEstrela == None:
-                nomeEstrela = "Desconhecido"
-            
-            
+            with open("nomePosicao.txt", "a") as nomePosicao:
+                nomePosicao.write(str(nomeEstrela) + " : " + str(posicao)+"\n")
+            with open("posicoes.txt", "a") as posicoes:
+                posicoes.write(str(posicao)+"\n")
+            pygame.draw.circle(espaco,branco,(x,y),5)
+
+
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_F11:
+            with open("posicoes.txt", "r") as posicoes:
+                for posicao in posicoes:
+                    x, y = ast.literal_eval(posicao)
+                    pygame.draw.circle(espaco,branco,(x,y),5)
+
+
+    
 
     tela.blit( espaco, (0, 0) )
     tela.blit(renderdotextoF10, (textoX, textoYF10))
     tela.blit(renderdotextoF11, (textoX, textoYF11))
     tela.blit(renderdotextoF12, (textoX, textoYF12))
 
+
     pygame.display.update()
+pygame.quit()
